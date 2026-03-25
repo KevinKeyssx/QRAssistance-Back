a520afd483486c73414582f269d95c593508b130
kevinkeyssx <keyss1700@gmail.com>
2026-03-25 12:15:52 -0300

    Qr entity field validation updated

--- entities/qr.py
+++ entities/qr.py
@@ -1,33 +1,35 @@
-from ulid           import ULID
-from mongoengine    import Document, StringField, DateTimeField, EnumField
-from datetime       import datetime
-from enum           import Enum
+from ulid       import ULID
+from pydantic   import Field
+from beanie     import Document
+from datetime   import datetime
+from enum       import Enum
 
 
-class QRType( Enum ):
-    QUORUM      = "quorum-elders"
-    SOCIETY     = "relief-society"
-    YOUNG       = "sunday-school-young"
-    ADULTS      = "sunday-school-adults"
-    MEN         = 'young-men'
-    WOMEN       = 'young-women'
-    JAS         = 'jas'
-    FRIENDS     = 'friends'
-    PRIMARY     = 'primary'
-    # Specials
-    FAMILY_HOME = 'family-home'
-    SHOW        = 'show'
-    EVENT       = 'event'
+class QRType( str, Enum ):
+	QUORUM      = "quorum-elders"
+	SOCIETY     = "relief-society"
+	YOUNG       = "sunday-school-young"
+	ADULTS      = "sunday-school-adults"
+	MEN         = 'young-men'
+	WOMEN       = 'young-women'
+	JAS         = 'jas'
+	FRIENDS     = 'friends'
+	PRIMARY     = 'primary'
+	# Specials
+	FAMILY_HOME = 'family-home'
+	SHOW        = 'show'
+	EVENT       = 'event'
 
 
 class QR( Document ):
-    meta = {'collection': 'qrs'}
+	id          : str       = Field( default_factory = lambda: str( ULID() ), alias = "_id" )
+	type        : QRType
+	date        : datetime
+	start_hour  : str
+	end_hour    : str
 
-    id          : str       = StringField( primary_key = True, default = lambda: str( ULID() ))
-    type        : QRType    = EnumField( QRType, required = True )
-    date        : datetime  = DateTimeField( required = True )
-    start_hour  : str       = StringField( required = True )
-    end_hour    : str       = StringField( required = True )
+	created_at  : datetime  = Field( default_factory = datetime.utcnow )
+	updated_at  : datetime  = Field( default_factory = datetime.utcnow )
 
-    created_at  : datetime  = DateTimeField( default = datetime.utcnow )
-    updated_at  : datetime  = DateTimeField( default = datetime.utcnow )
+	class Settings:
+		name = "qrs"

d5d9e4acfc467cbbcf33f149e00c14d2c0bf3321
kevinkeyssx <keyss1700@gmail.com>
2026-03-25 12:15:09 -0300

    QR & Member implemented

--- main.py
+++ main.py
@@ -3,34 +3,37 @@ from fastapi                    import FastAPI
 from fastapi.middleware.cors    import CORSMiddleware
 
 # Routers
-from routers.dynamicform import dynamicform
+from routers.qr_router      import qr_router
+from routers.member_router  import member_router
 
 # Services
-from services.form_service import init_db
+from services.qr_service        import init_db
+from services.member_service    import init_db
 
 # New FastAPI
 app = FastAPI(
-    title       = 'QR Assistance',
-    description = 'Asistencia por QR',
-    version     = '0.0.1'
+	title       = 'QR Assistance',
+	description = 'Asistencia por QR',
+	version     = '0.0.1'
 )
 
 # !Only dev
 # CORSMiddleware
-origins = ["*"]
+origins = [ "*" ]
 
 # middleware
 app.add_middleware(
-    CORSMiddleware,
-    allow_origins       = origins,
-    allow_credentials   = True,
-    allow_methods       = ["*"],
-    allow_headers       = ["*"],
+	CORSMiddleware,
+	allow_origins       = origins,
+	allow_credentials   = True,
+	allow_methods       = [ "*" ],
+	allow_headers       = [ "*" ],
 )
 
-@app.on_event("startup")
+@app.on_event( "startup" )
 async def start_database():
-    await init_db()
+	await init_db()
 
 # include_router
-app.include_router( dynamicform )
+app.include_router( qr_router )
+app.include_router( member_router )

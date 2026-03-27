37b34057928582df13d283381abfbe34ebd6a8cb
kevinkeyssx <keyss1700@gmail.com>
2026-03-26 23:53:11 -0300

    ulid_token implemented

--- dtos/member_dto.py
+++ dtos/member_dto.py
@@ -6,24 +6,23 @@ from datetime   import datetime
 from dtos.paginated_dto import PaginatedResponse
 
 
-class MemberCreateDTO(BaseModel):
-    name        : str = Field(..., min_length=2, max_length=50, description="Nombre del miembro")
-    last_name   : str = Field(..., min_length=2, max_length=50, description="Apellido del miembro")
+class MemberCreateDTO( BaseModel ):
+    name        : str       = Field(..., min_length=2, max_length=50, description="Nombre del miembro")
+    last_name   : str       = Field(..., min_length=2, max_length=50, description="Apellido del miembro")
     classes     : List[str] = Field(default_factory=list, description="Clases del miembro")
-    ulid_token  : str = Field(..., min_length=26, max_length=26, description="Token ULID único del miembro")
-    saveFinger  : bool = Field(default=False, description="Indica si se debe guardar la huella digital del miembro")
+    saveFinger  : bool      = Field(default=False, description="Indica si se debe guardar la huella digital del miembro")
 
 
-class MemberUpdateDTO(BaseModel):
+class MemberUpdateDTO( BaseModel ):
     name        : Optional[str]         = Field(None, min_length=2, max_length=50, description="Nombre del miembro")
     last_name   : Optional[str]         = Field(None, min_length=2, max_length=50, description="Apellido del miembro")
     classes     : Optional[List[str]]   = Field(None, description="Clases del miembro")
-    ulid_token  : Optional[str]         = Field(None, min_length=26, max_length=26, description="Token ULID único del miembro")
     saveFinger  : Optional[bool]        = Field(None, description="Indica si se debe guardar la huella digital del miembro")
 
 
-class MemberReadDTO(MemberCreateDTO):
-    id          : str = Field(..., alias="_id")
+class MemberReadDTO( MemberCreateDTO ):
+    id          : str = Field( ..., alias = "_id" )
+    ulid_token  : str 
     created_at  : datetime
     updated_at  : datetime
 

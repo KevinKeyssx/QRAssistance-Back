2571088aaf27497807c54e34c4a51456dfad0001
kevinkeyssx <keyss1700@gmail.com>
2026-03-26 23:53:50 -0300

    Member code updated

--- entities/member.py
+++ entities/member.py
@@ -6,15 +6,15 @@ from typing     import List
 
 
 class Member( Document ):
-    id          : str = Field( default_factory=lambda: str(ULID()), alias="_id" )
-    name        : str
-    last_name   : str
-    classes     : List[str] = []
-    ulid_token  : str
-    saveFinger  : bool = False
+    id          : str       = Field( default_factory = lambda: str( ULID() ), alias = "_id" )
+    name        : str       = Field( ..., min_length = 2, max_length = 50 )
+    last_name   : str       = Field( ..., min_length = 2, max_length = 50 )
+    classes     : List[str] = Field( default_factory = list )
+    ulid_token  : str       = Field( default_factory = lambda: str( ULID() ) )
+    saveFinger  : bool      = Field( default = False )
 
-    created_at  : datetime = Field( default_factory=datetime.utcnow )
-    updated_at  : datetime = Field( default_factory=datetime.utcnow )
+    created_at  : datetime  = Field( default_factory = datetime.utcnow )
+    updated_at  : datetime  = Field( default_factory = datetime.utcnow )
 
     class Settings:
         name = "members"

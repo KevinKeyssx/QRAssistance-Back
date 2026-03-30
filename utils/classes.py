6c215fa8bcd48a7f98727b34bde72499e4d81ef8
kevinkeyssx <keyss1700@gmail.com>
2026-03-29 22:00:57 -0300

    LDS_CLASSES implemented

---
+++ utils/classes.py
@@ -0,0 +1,10 @@
+LDS_CLASSES = [
+    {"slug": "quorum-elders", "label": "Quórum de élderes"},
+    {"slug": "relief-society", "label": "Sociedad de socorro"},
+    {"slug": "sunday-school-young", "label": "Escuela dominical jóvenes"},
+    {"slug": "sunday-school-adults", "label": "Escuela dominical adúltos"},
+    {"slug": "young-men", "label": "Hombres jóvenes"},
+    {"slug": "young-women", "label": "Mujeres jóvenes"},
+    {"slug": "jas", "label": "JAS"},
+    {"slug": "friends", "label": "Clase amigos"},
+]

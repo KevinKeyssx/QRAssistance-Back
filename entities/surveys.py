06d880880a98642a0ab587c7370375ae70518cb4
kevinkeyssx <keyss1700@gmail.com>
2026-04-04 23:51:05 -0400

    Member entity implemented

--- entities/surveys.py
+++ entities/surveys.py
@@ -1,14 +1,18 @@
-from beanie             import Document, Link
-from pydantic           import Field
-from ulid               import ULID
-from datetime           import datetime
+from beanie         import Document, Link
+from pydantic       import Field
+from ulid           import ULID
+from datetime       import datetime
+
+# Entities
 from entities.qr        import QR
+from entities.member    import Member
 
 
 class Survey( Document ):
     id          : str       = Field( default_factory=lambda: str(ULID()), alias="_id" )
 
     qr          : Link[QR]
+    member      : Link[Member]
     question1   : bool  = Field( default=False )
     question2   : bool  = Field( default=False )
     question3   : bool  = Field( default=False )

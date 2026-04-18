4ba6ba979dc98b4fac6d90f8641d481feed3cc4a
kevinkeyssx <keyss1700@gmail.com>
2026-04-18 03:14:36 -0400

    session_id implemented & TAbs updated

--- dtos/qr_dto.py
+++ dtos/qr_dto.py
@@ -5,33 +5,34 @@ from entities.qr        import QRType
 from dtos.paginated_dto import PaginatedResponse
 
 
-class QRCreateDTO(BaseModel):
-    type        : QRType    = Field(..., description="Tipo de QR")
-    date        : datetime  = Field(..., description="Fecha del QR")
-    start_hour  : str       = Field(..., min_length = 5, max_length = 5, example = "09:00", description="Hora de inicio del QR")
-    end_hour    : str       = Field(..., min_length = 5, max_length = 5, example = "10:30", description="Hora de fin del QR")
+class QRCreateDTO( BaseModel ):
+	type        : QRType   = Field( ..., description = "Tipo de QR" )
+	date        : datetime = Field( ..., description = "Fecha del QR" )
+	start_hour  : str      = Field( ..., min_length = 5, max_length = 5, example = "09:00", description = "Hora de inicio del QR" )
+	end_hour    : str      = Field( ..., min_length = 5, max_length = 5, example = "10:30", description = "Hora de fin del QR" )
 
 
-class QRUpdateDTO(BaseModel):
-    type        : Optional[QRType]      = Field(None, description="Tipo de QR")
-    date        : Optional[datetime]    = Field(None, description="Fecha del QR")
-    start_hour  : Optional[str]         = Field(None, min_length = 5, max_length = 5, description="Hora de inicio del QR")
-    end_hour    : Optional[str]         = Field(None, min_length = 5, max_length = 5, description="Hora de fin del QR")
+class QRUpdateDTO( BaseModel ):
+	type        : Optional[ QRType ]   = Field( None, description = "Tipo de QR" )
+	date        : Optional[ datetime ] = Field( None, description = "Fecha del QR" )
+	start_hour  : Optional[ str ]      = Field( None, min_length = 5, max_length = 5, description = "Hora de inicio del QR" )
+	end_hour    : Optional[ str ]      = Field( None, min_length = 5, max_length = 5, description = "Hora de fin del QR" )
 
 
 class QRReadDTO( QRCreateDTO ):
-    id: str = Field(..., alias="_id", description="ID del QR")
-    created_at: datetime = Field(..., description="Fecha de creación del QR")
-    updated_at: datetime = Field(..., description="Fecha de actualización del QR")
+	id          : str      = Field( ..., alias = "_id", description = "ID del QR" )
+	session_id  : str      = Field( ..., description = "ID de sesión del QR" )
+	created_at  : datetime = Field( ..., description = "Fecha de creación del QR" )
+	updated_at  : datetime = Field( ..., description = "Fecha de actualización del QR" )
 
-    class Config:
-        populate_by_name = True
-        from_attributes = True
+	class Config:
+		populate_by_name = True
+		from_attributes  = True
 
 
 class QRWithCountDTO( QRReadDTO ):
-    assist_count: int = Field(0, description="Cantidad de asistencias del QR")
+	assist_count : int = Field( 0, description = "Cantidad de asistencias del QR" )
 
 
-class PaginatedQRResponse( PaginatedResponse[QRWithCountDTO] ):
-    pass
+class PaginatedQRResponse( PaginatedResponse[ QRWithCountDTO ] ):
+	pass

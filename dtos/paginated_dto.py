a6dc4b75597354c023402a7d78859502bc5e195c
kevinkeyssx <keyss1700@gmail.com>
2026-04-19 09:25:32 -0400

    Size 1000

--- dtos/paginated_dto.py
+++ dtos/paginated_dto.py
@@ -24,6 +24,6 @@ class Pagination( BaseModel ):
     size: int = Query(
         10,
         ge          = 1,
-        le          = 100,
+        le          = 1000,
         description = "Elementos por página"
     )

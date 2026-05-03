aa2c7cdfaaa69d28ec8e0d13ff9f6eddd65ce85b
kevinkeyssx <keyss1700@gmail.com>
2026-05-03 00:11:15 -0400

    Envs added

---
+++ utils/envs.py
@@ -0,0 +1,20 @@
+# Python
+import os
+from dotenv import load_dotenv
+
+# Load .env file
+load_dotenv( dotenv_path = ".env" )
+
+# Security
+INTERNAL_SECRET_KEY = os.getenv( "INTERNAL_SECRET_KEY" )
+GOOGLE_CLIENT_ID    = os.getenv( "GOOGLE_CLIENT_ID" )
+
+# Configuration
+TIMEZONE            = os.getenv( "TIMEZONE" )
+ORIGINS             = os.getenv( "ORIGINS", "*" )
+START_HOUR          = os.getenv( "START_HOUR" )
+END_HOUR            = os.getenv( "END_HOUR" )
+
+# Database
+MONGONAME           = os.getenv( "MONGONAME" )
+MONGO_PUBLIC_URL    = os.getenv( "MONGO_PUBLIC_URL" )
